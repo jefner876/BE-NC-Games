e7ed2b82e62e43a9a7c6ac966f80e53d85773ad4
jefner876 <jefner876@hotmail.com>
2023-07-24 14:48:44 +0100

    update for elephantsql url

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,15 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 const config = {};
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
